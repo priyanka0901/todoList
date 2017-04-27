5dd9f3faf87d9995491bb83e3a8831827a7f23b6
priyanka0901 <gupta.priyanka0901@gmail.com>
2017-04-27 11:00:26 +0530

    initital commit of started kit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { IndexLink, Link } from 'react-router'
+import './Header.scss'
+
+export const Header = () => (
+  <div>
+    <h1>React Redux Starter Kit</h1>
+    <IndexLink to='/' activeClassName='route--active'>
+      Home
+    </IndexLink>
+    {' · '}
+    <Link to='/counter' activeClassName='route--active'>
+      Counter
+    </Link>
+    <Link to='/calculator' activeClassName='route--active'>
+     Calculator
+    </Link>
+    <Link to='/result' activeClassName='route--active'>
+     Result
+    </Link>
+  </div>
+)
+
+export default Header
